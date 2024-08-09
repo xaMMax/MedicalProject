5d658b3167aef07977263e6436847716d1f5af84
xaMMax <petrolijuice@gmail.com>
2024-08-09 15:52:38 +0200

    add rest_framework_api key

--- consultation_app/consultation_app/settings.py
+++ consultation_app/consultation_app/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'csp',
     'rest_framework',
+    'rest_framework_api_key',
     'rest_framework_simplejwt',
     'rest_framework_simplejwt.token_blacklist',
     'drf_yasg',
