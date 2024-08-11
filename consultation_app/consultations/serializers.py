7a38777c59de40be0c270008c313646315aaeaef
xaMMax <petrolijuice@gmail.com>
2024-08-11 19:02:31 +0200

    fix serializers.py

--- consultation_app/consultations/serializers.py
+++ consultation_app/consultations/serializers.py
@@ -30,12 +30,17 @@ class CustomUserSerializer(serializers.ModelSerializer):
         return user
 
 
-class UserProfileSerializer(serializers.ModelSerializer, CustomUserSerializer):
+class UserProfileSerializer(serializers.ModelSerializer):
     groups = serializers.StringRelatedField(many=True)
     user_permissions = serializers.StringRelatedField(many=True)
 
     class Meta:
         model = CustomUser
+        fields = ['username', 'first_name', 'last_name', 'email', 'is_doctor', 'is_user', 'phone', 'address', 'bio',
+                  'photo', 'groups', 'user_permissions']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
 
 
 class ChangePasswordSerializer(serializers.Serializer):
