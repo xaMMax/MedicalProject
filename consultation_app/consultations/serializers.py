5787c6a5d7a42760506977fc33af850f28278cd6
xaMMax <petrolijuice@gmail.com>
2024-08-11 18:21:39 +0200

    fix serializers.py

--- consultation_app/consultations/serializers.py
+++ consultation_app/consultations/serializers.py
@@ -12,11 +12,8 @@ CustomUser = get_user_model()
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
-        fields = [
-            'id', 'username', 'email', 'is_doctor', 'is_user',
-            'phone', 'address', 'bio', 'photo', 'groups',
-            'user_permissions'
-        ]
+        fields = ['username', 'first_name', 'last_name', 'email', 'is_doctor', 'is_user', 'phone', 'address', 'bio',
+                  'photo', 'groups', 'user_permissions']
         extra_kwargs = {
             'password': {'write_only': True}
         }
@@ -32,25 +29,13 @@ class CustomUserSerializer(serializers.ModelSerializer):
 
 
 class UserProfileSerializer(serializers.ModelSerializer):
+    groups = serializers.StringRelatedField(many=True)
+    user_permissions = serializers.StringRelatedField(many=True)
+
     class Meta:
         model = CustomUser
-        fields = [
-            'id', 'username', 'email', 'is_doctor', 'is_user',
-            'phone', 'address', 'bio', 'photo', 'groups',
-            'user_permissions'
-        ]
-        extra_kwargs = {
-            'password': {'write_only': True}
-        }
-
-    def create(self, validated_data):
-        user = CustomUser.objects.create_user(
-            email=validated_data['email'],
-            username=validated_data['username'],
-            password=validated_data['password'],
-            **validated_data
-        )
-        return user
+        fields = ['username', 'first_name', 'last_name', 'email', 'is_doctor', 'is_user', 'phone', 'address', 'bio',
+                  'photo', 'groups', 'user_permissions']
 
 
 class ChangePasswordSerializer(serializers.Serializer):
