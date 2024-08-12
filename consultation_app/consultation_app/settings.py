24e5d95e5af3e01699f58a0da7343ccfbf544412
xammax <petrolijuice@gmail.com>
2024-08-12 11:54:15 +0200

    update settings.py

--- consultation_app/consultation_app/settings.py
+++ consultation_app/consultation_app/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 
-ALLOWED_HOSTS = ['xammax.pythonanywhere.com', 'localhost', '127.0.0.1', '192.168.147.4', "192.168.1.12"]
+ALLOWED_HOSTS = ['xammax.pythonanywhere.com', 'localhost', '127.0.0.1', '192.168.147.4', "192.168.1.12",]
 CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
@@ -173,7 +173,9 @@ X_FRAME_OPTIONS = 'DENY'  # DENY
 CORS_ALLOWED_ORIGINS = [
     "https://localhost:3000",
     "https://xammax.pythonanywhere.com",
-    "http://192.168.1.12:8080"
+    "http://192.168.1.12:8080",
+    "http://192.168.1.12:3000",
+    "https://master--consultationsproj.netlify.app"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
