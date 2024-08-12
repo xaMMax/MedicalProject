e18b39d74fa7ca9e50b3a438249d4883cae028e0
xammax <petrolijuice@gmail.com>
2024-08-12 19:37:06 +0200

    update

--- consultation_app/consultations/serializers.py
+++ consultation_app/consultations/serializers.py
@@ -17,7 +17,7 @@ class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = CustomUser
         fields = ['id', 'username', 'first_name', 'last_name', 'email',
-                  'phone', 'address', 'bio', 'photo', 'is_superuser', 'is_doctor', 'is_user']
+                  'phone', 'address', 'bio', 'photo', 'is_superuser', 'is_doctor', 'is_user', 'is_staff']
         extra_kwargs = {
             'password': {'write_only': True}
         }
