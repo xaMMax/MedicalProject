b221c223630af863ba0bca426055e1517516b2c8
xammax <petrolijuice@gmail.com>
2024-08-12 16:31:48 +0200

    update settings.py

--- consultation_app/consultation_app/settings.py
+++ consultation_app/consultation_app/settings.py
@@ -172,13 +172,13 @@ ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
     '192.168.1.12',
-    'consul.netlify.app',
+    'https://xammax.github.io/medical-project/',
 ]
 
 CORS_ALLOWED_ORIGINS = [
     "http://192.168.1.12:3000",
-    "https://consul.netlify.app",
     "https://xammax.pythonanywhere.com",
+    "https://xammax.github.io/medical-project/"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
